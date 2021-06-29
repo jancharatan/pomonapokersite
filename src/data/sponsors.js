f09f349b5f66c6be3ae6be0fcb5a3c74ad2ecfc3
Jan Charatan <jan.charatan@gmail.com>
2021-06-28 19:27:49 -0700

    Added two sigma.

--- src/data/sponsors.js
+++ src/data/sponsors.js
@@ -4,5 +4,12 @@ export const sponsors = [
         image: "assets/sponsorLogos/numeradeLogo.png",
         link: "https://www.numerade.com",
         description: "Numerade is a venture-backed, high-growth education technology startup based in Los Angeles."
+    },
+    {
+        companyName: "Two Sigma",
+        image: "assets/sponsorLogos/twoSigmaLogo.png",
+        link: "https://www.twosigma.com",
+        description: "Two Sigma's scientists bring rigorous inquiry, data analysis, and invention to help solve the toughest challenges across financial services."
+
     }
 ];
